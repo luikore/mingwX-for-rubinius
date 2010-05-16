46088ef587803ce248ae99ba24d2ebbdef7cf82e
luikore <usurffx@gmail.com>
2010-05-17 01:32:50 +0800

    change wait and resource

--- include/sys/resource.h
+++ include/sys/resource.h
@@ -1,80 +1,25 @@
-/*C+
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * 
- * This Software Source is the unpublished, valuable, confidential property
- * of Microsoft Corporation.  Use is authorised only under the terms and
- * conditions set out in the source code licence agreement protecting this
- * Software Source.  Any unauthorised use or disclosure of this Software 
- * Source is prohibited.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' BY MICROSOFT CORPORATION AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL MICROSOFT CORPORATION BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *C-
- */ 
-
-/* RCSid = $Header: /E/interix/include/sys/resource.h,v 1.8 1999/05/07 21:02:27 mark Exp $
- */
-
 #ifndef _SYS_RESOURCE_H
 #define _SYS_RESOURCE_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif 
-
-#include <sys/types.h>		/* for id_t */
 #include <sys/time.h>		/* for struct timeval */
 
-/* 
- * process priority constants (for setpriority() and getpriority())
- */
-#define PRIO_PROCESS	0
-#define PRIO_PGRP	1
-#define PRIO_USER	2
-
-#define PRIO_MIN	-2
-#define PRIO_MAX	2
-
-//extern int _CRTAPI1 getpriority(int which, id_t who);
-//extern int _CRTAPI1 setpriority(int which, id_t who, int prio);
-
-/*
- * process resource limits definitions
- */
-typedef unsigned long long rlim_t;
+#define RLIMIT_CPU	0	/* cpu time in milliseconds */
+#define RLIMIT_FSIZE	1	/* maximum file size */
+#define RLIMIT_DATA	2	/* data size */
+#define RLIMIT_STACK	3	/* stack size */
+#define RLIMIT_CORE	4	/* core file size */
+#define RLIMIT_RSS	5	/* resident set size */
+#define RLIMIT_MEMLOCK	6	/* locked-in-memory address space */
+#define RLIMIT_NPROC	7	/* number of processes */
+#define RLIMIT_NOFILE	8	/* number of open files */
+#define RLIM_NLIMITS	9	/* number of resource limits */
+#define RLIM_INFINITY	((long) ((1UL << 31) - 1UL))
 
 struct rlimit {
-	rlim_t  rlim_cur;
-	rlim_t  rlim_max;
+	long  rlim_cur;
+	long  rlim_max;
 };
-
 typedef struct rlimit  rlimit_t;
-
-#define RLIM_INFINITY	((rlim_t)(-1))
-
-#define RLIMIT_CPU	0   /* limit on CPU time per process */
-#define RLIMIT_FSIZE	1   /* limit on file size */
-#define RLIMIT_DATA	2   /* limit on data segment size */
-#define RLIMIT_STACK	3   /* limit on process stack size */
-#define RLIMIT_CORE	4   /* limit on size of core dump file */
-#define RLIMIT_NOFILE	5   /* limit on number of open files */
-#define RLIMIT_AS	6   /* limit on process total address space size */
-#define RLIMIT_VMEM	RLIMIT_AS
-
-#define RLIM_NLIMITS	7
-
-//int _CRTAPI1 getrlimit(int, struct rlimit *);
-//int _CRTAPI1 setrlimit(int, const struct rlimit *);
-
 
 struct  rusage {
         struct timeval ru_utime;        /* user time used */
@@ -100,7 +45,13 @@ struct  rusage {
 #define RUSAGE_SELF 	0
 #define RUSAGE_CHILDREN	1
 
-//int __cdecl getrusage(int, struct rusage *);
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+extern int __cdecl getrusage(int, struct rusage *);
+extern int __cdecl getrlimit(int, struct rlimit *);
+extern int __cdecl setrlimit(int, const struct rlimit *);
 
 #ifdef __cplusplus
 }
--- include/sys/wait.h
+++ include/sys/wait.h
@@ -1,21 +1,30 @@
 #ifndef _SYS_WAIT_H
 #define _SYS_WAIT_H
 
-static int wait(int *x)
-{
-	return -1;
-}
+#define WIFEXITED(w)	(((w) & 0xff) == 0)
+#define WIFSIGNALED(w)	(((w) & 0x7f) > 0 && (((w) & 0x7f) < 0x7f))
+#define WIFSTOPPED(w)	(((w) & 0xff) == 0x7f)
+#define WEXITSTATUS(w)	(((w) >> 8) & 0xff)
+#define WTERMSIG(w)	((w) & 0x7f)
+#define WSTOPSIG	WEXITSTATUS
+#define WIFCONTINUED(stat_val)	0
+#define WNOHANG			1
+#define WCONTINUED		0
+#define WUNTRACED		0
 
-static int waitpid(int x, int *y, int z)
-{
-	return -1;
-}
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#define WNOHANG 1
-#define WEXITSTATUS(X) ((unsigned)(X) >> 8)
-#define WIFEXITED(X) (((X) & 255) == 0)
-#define WIFSIGNALED(X) ((((X) & 255) != 0x255 && ((X) & 255) != 0))
-#define WTERMSIG(X) ((X) & 255)
+typedef int pid_t;
+
+extern pid_t __cdecl wait(int *stat_loc);
+extern pid_t __cdecl waitpid(pid_t pid, int *stat_loc, int options);
+extern pid_t __cdecl wait3(int *, int, struct rusage *);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
